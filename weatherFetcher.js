dabb693ebd822a806492edc57905684dede276df
KARUNAKARAN  VIJAYARAGHAVAN <66834895+karunakaran186@users.noreply.github.com>
2023-01-23 23:56:38 +0530

    Update weatherFetcher.js

--- weatherFetcher.js
+++ weatherFetcher.js
@@ -32,7 +32,7 @@ let weather = {
         "Wind speed: " + speed + " km/h";
       document.querySelector(".weather").classList.remove("loading");
       document.body.style.backgroundImage =
-        "url('https://unsplash.com/s/photos/weather/?" + name + "')";
+        "url('https://source.unsplash.com/1600x900/?" + weather+ "')";
     },
     search: function () {
       this.fetchWeather(document.querySelector(".search-bar").value);
